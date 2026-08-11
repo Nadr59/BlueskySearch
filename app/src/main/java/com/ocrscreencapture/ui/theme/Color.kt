d72fe96fd09121d189c4cfb77cd0f539da36c5e7
Nadr59 <117353272+Nadr59@users.noreply.github.com>
2026-08-11 12:57:22 +0300

    Add Color.kt with theme color definitions

---
+++ app/src/main/java/com/ocrscreencapture/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.ocrscreencapture.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Green500 = Color(0xFF4CAF50)
+val Green700 = Color(0xFF388E3C)
+val DarkBg = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E1E)
+val DarkSurfaceVariant = Color(0xFF2C2C2C)
+val OnDark = Color(0xFFE0E0E0)
