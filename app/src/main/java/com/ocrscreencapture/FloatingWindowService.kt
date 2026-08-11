23aa0e9c8a15bf2eeccc3f0ecdaa0b61059c24e5
Nadr59 <117353272+Nadr59@users.noreply.github.com>
2026-08-11 21:34:13 +0300

    Add Bitmap import to FloatingWindowService.kt

--- app/src/main/java/com/ocrscreencapture/FloatingWindowService.kt
+++ app/src/main/java/com/ocrscreencapture/FloatingWindowService.kt
@@ -7,6 +7,7 @@ import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.content.pm.ServiceInfo
+import android.graphics.Bitmap
 import android.graphics.PixelFormat
 import android.graphics.RectF
 import android.os.Build
